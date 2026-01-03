6de65b39db5c814e19707f009f3b27a7332538e9
notcancername <notcancername@protonmail.com>
2026-01-03 06:43:07 +0100

    switch to managed memorypool

--- src/core/task.zig
+++ src/core/task.zig
@@ -474,7 +474,7 @@ pub fn resumeTask(obj: anytype, comptime mode: ResumeMode) void {
 pub const TaskPool = struct {
     pub const pool_item_size = std.mem.alignForward(usize, @sizeOf(AnyTask) + 128, 128);
 
-    pool: std.heap.MemoryPoolAligned([pool_item_size]u8, .fromByteUnits(Closure.task_alignment)),
+    pool: std.heap.memory_pool.AlignedManaged([pool_item_size]u8, .fromByteUnits(Closure.task_alignment)),
     mutex: std.Thread.Mutex = .{},
 
     pub fn init(allocator: std.mem.Allocator) TaskPool {
